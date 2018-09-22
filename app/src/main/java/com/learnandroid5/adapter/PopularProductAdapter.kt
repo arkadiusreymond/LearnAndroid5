ca650328ed4fe4d6ac41c71928aafd3f8dde43f9
Arkadius Reymond <arkadiusreymond@gmail.com>
2018-09-22 10:38:54 +0700

    no message

--- app/src/main/java/com/learnandroid5/adapter/PopularProductAdapter.kt
+++ app/src/main/java/com/learnandroid5/adapter/PopularProductAdapter.kt
@@ -9,21 +9,23 @@ import android.widget.Toast
 import com.learnandroid5.R
 import com.learnandroid5.model.Product
 
-class ProductsListAdapter(val products : ArrayList<Product>) : RecyclerView.Adapter<ProductsListAdapter.MovieViewHolder>() {
-    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+class PopularProductAdapter(val products : ArrayList<Product>) : RecyclerView.Adapter<PopularProductAdapter.ViewHolder>() {
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(products.get(position))
     }
 
     override fun getItemCount() = products.size
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.product_item, parent, false)
-        return MovieViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.popular_product_item, parent, false)
+        return ViewHolder(view)
     }
 
-    class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
         private var view : View = itemView
         private var product : Product? = null
+        var productName = view.findViewById(R.id.product_name) as TextView
 
         override fun onClick(p0: View?) {
             Toast.makeText(view.context, "Item diklik", Toast.LENGTH_LONG).show()
@@ -35,10 +37,9 @@ class ProductsListAdapter(val products : ArrayList<Product>) : RecyclerView.Adap
 
         fun bind(product: Product) {
             this.product = product
+            productName.text = product.name
 //            val imageUrl = StringBuilder()
 //            imageUrl.append(view.context.getString(R.string.base_path_poster)).append(popular.posterPath)
-            var productName = view.findViewById(R.id.nama_product) as TextView
-            productName.text = product.name
 //            view.nama_product.setText(product.name)
 //            Log.d("sad",popular.campign_id.toString())
 //            Glide.with(view.context).load(imageUrl.toString()).into(view.mvPoster)
---
+++ app/src/main/java/com/learnandroid5/adapter/PopularSectionAdapter.kt
@@ -0,0 +1,48 @@
+package com.learnandroid5.adapter
+
+import android.content.Context
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.OrientationHelper
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import com.learnandroid5.R
+import com.learnandroid5.model.Popular
+
+class PopularSectionAdapter(val populars : ArrayList<Popular>, var context: Context) : RecyclerView.Adapter<PopularSectionAdapter.ViewHolder>() {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(populars.get(position), context)
+    }
+
+    override fun getItemCount() = populars.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.popular_section, parent, false)
+        return ViewHolder(view)
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        private var view : View = itemView
+        private var popular : Popular? = null
+        private var rvPopularProduct = view.findViewById(R.id.rvPopularProducts) as RecyclerView
+        private var popularTitle = view.findViewById(R.id.populars_title_list) as TextView
+
+        override fun onClick(p0: View?) {
+            Toast.makeText(view.context, "Item diklik", Toast.LENGTH_LONG).show()
+        }
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        fun bind(popular: Popular, context: Context) {
+            this.popular = popular
+            popularTitle.setText(popular.title)
+            rvPopularProduct.layoutManager = LinearLayoutManager(context, OrientationHelper.HORIZONTAL, false)
+            rvPopularProduct.adapter = PopularProductAdapter(popular.products)
+        }
+    }
+}
